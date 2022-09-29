acbb380b5c0ba7fe7fc918b8239d07f4ccc6a339
Sejin <kisejin2015@gmail.com>
2022-09-29 11:08:03 -0600

    routes

--- models/index.js
+++ models/index.js
@@ -5,12 +5,17 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category,{foreignKey:"category_id"})
 
 // Categories have many Products
+Category.hasMany(Product,{foreignKey:"category_id"})
 
-// Products belongToMany Tags (through ProductTag)
+// Products belongToMany Tags (trough ProductTag)
+Product.belongsToMany(Tag,{foreignKey:"product_id", through: ProductTag })
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{foreignKey:"tag_id", through: ProductTag })
+
 
 module.exports = {
   Product,
